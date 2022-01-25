00147303e4d4dcf2fe08e52318c3b6af7c761e5f
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-01-25 15:18:33 +0100

    P2Comments assignment

---
+++ P2Comments/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Name: Lucas Olsson
+ Date: 25 jan 2022
+ Mail: lucas.olsson@student.forsbergsskola.se */
+
+namespace P2Comments
+{
+    public class program
+    {
+        /// A Main Method is the first method that starts when you run the application
+        public static void Main()
+        {
+            //Prints out the line "Hello World"
+            Console.WriteLine("Hello World");
+        }
+    }
+}
