0f5940b65bb9298ca8e249758330863ccb38b9b1
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-01-26 15:47:05 +0100

    P9Strings assignment

---
+++ Assignments-003.3.1-Console-Basics-2/P9Strings/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What's your first name?"); 
+string firstname = Console.ReadLine(); 
+Console.WriteLine("What's your last name?"); 
+string lastname = Console.ReadLine(); 
+Console.WriteLine("What's your age?"); 
+string age = Console.ReadLine();
+
+Console.WriteLine("Hello " + firstname + " " + lastname + ", " + "you are " + age + " years old!");
+            
