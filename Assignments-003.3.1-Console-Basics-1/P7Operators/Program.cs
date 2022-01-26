0d8ac53b6fc2ce1542bbe3a3fc7091c70268a8c1
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-01-26 14:20:46 +0100

    P7Operators assignment

---
+++ Assignments-003.3.1-Console-Basics-1/P7Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿
+        int a = 7;
+        int b = 5; 
+        int mult = 3; 
+        int sub = 6; 
+        double div = 1.2;
+        int mod = 17;
+
+        Console.WriteLine(((a + b) * mult - sub) / div % mod);
+
+
