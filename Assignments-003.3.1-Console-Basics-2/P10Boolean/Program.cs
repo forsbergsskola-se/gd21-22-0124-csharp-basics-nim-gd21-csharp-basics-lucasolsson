65a21292f332c1d592bfdab7daaa9842d64bc558
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-01-26 16:20:51 +0100

    P10Boolean assignment

---
+++ Assignments-003.3.1-Console-Basics-2/P10Boolean/Program.cs
@@ -0,0 +1,26 @@
+﻿bool isChild = false;
+bool isTeenager = false;
+bool isGrownup = false;
+
+Console.WriteLine("What's your age?");
+string ageInput = Console.ReadLine();
+int age = Convert.ToInt32(ageInput);
+
+if (age >= 0 && age <= 12)
+{
+    isChild = true;
+}
+
+if (age >= 13 && age <= 19)
+{
+    isTeenager = true;
+}
+
+if (age > 12)
+{
+    isGrownup = true;
+}
+
+Console.WriteLine("You are a child:" + isChild);
+Console.WriteLine("You are a teenager:" + isTeenager);
+Console.WriteLine("You are a grown-up:" + isGrownup);
