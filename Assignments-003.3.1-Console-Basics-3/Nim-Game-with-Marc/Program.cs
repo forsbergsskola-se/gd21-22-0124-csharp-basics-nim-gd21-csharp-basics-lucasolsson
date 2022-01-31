1b7011a8c9926ac9c93087436aae2689a96ff2d9
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-01-31 10:10:37 +0100

    Nim game with Marc

---
+++ Assignments-003.3.1-Console-Basics-3/Nim-Game-with-Marc/Program.cs
@@ -0,0 +1,73 @@
+﻿int players = 2;
+int matches = 24;
+int minMatches = 1;
+int maxMatches = 3;
+
+
+Console.WriteLine("Welcome to Nim!");
+
+while (!IsGameOver())
+{
+    PrintMatches();
+    PlayerDraw();
+    
+    if (matches < 1)
+    {
+        Console.WriteLine("You lose");
+    }
+    else
+    {
+        PrintMatches();
+        AIDraw();
+        
+        if (matches < 1)
+        {
+            Console.WriteLine("AI lose");
+        }
+    }
+}
+
+//Functions for the game
+void PrintMatches()
+{
+    //Print matches  
+    for (int i = 0; i < matches; i++)
+    {
+        Console.Write("|");
+    }
+    //New line for not getting everything on same line 
+    Console.WriteLine($" {matches} ");
+}
+
+void PlayerDraw()                                                      
+{                                                                      
+    int inputNumber = 0;                                               
+    while (inputNumber < minMatches || inputNumber > maxMatches)       
+    {                                                                  
+        Console.WriteLine("How many matches do you want to draw?");    
+        string inputText = Console.ReadLine();                         
+        inputNumber = int.Parse(inputText);                            
+    }                                                                  
+    matches -= inputNumber;                                            
+}                                                                      
+
+void AIDraw()
+{
+    int inputNumber = 3;
+    Console.WriteLine($"The AI draws {inputNumber} matches");
+    matches -= inputNumber;
+}
+
+bool IsGameOver()
+{
+    return matches < 1;
+}
+
+
+
+
+
+
+
+
+
